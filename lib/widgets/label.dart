ef5eb571e8bec9342281bc5034fa1d6235b915a1
Anas MASSNAOUI <anasmassnaoui@gmail.com>
2021-09-30 19:46:05 +0100

    remove unused code

--- lib/widgets/label.dart
+++ lib/widgets/label.dart
@@ -12,22 +12,12 @@ class Label extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextButton(
-        onPressed: onTap,
-        style: ButtonStyle(
-            foregroundColor: MaterialStateProperty.all(Colors.white)),
-        child: Container(
-          padding: EdgeInsets.symmetric(vertical: 10),
-          child: Text(text),
-        ));
-    return GestureDetector(
-      onTap: onTap,
-      child: Expanded(
-        child: Container(
-          padding: EdgeInsets.all(20),
-          child: Center(
-            child: Text(this.text),
-          ),
-        ),
+      onPressed: onTap,
+      style:
+          ButtonStyle(foregroundColor: MaterialStateProperty.all(Colors.white)),
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 10),
+        child: Text(text),
       ),
     );
   }
